a275681b511e2921795f1664146dc89ff17d0171
danilo <radomirovic.danilo17@gmail.com>
2022-02-22 20:44:00 +0100

    Kreirane klase i operacije

---
+++ klase/automobil.php
@@ -0,0 +1,40 @@
+<?php
+class Igrac{
+    private $brend;
+    private $model;
+    private $opis;
+    private $url;
+    private $vlasnik;
+    private $karoserija;
+    public function __construct(string $brend, strin $model, string $opis, string $url, Korsnik $vlasnik, Karoserija $karoserija)
+    {
+        $this->brend=$brend;
+        $this->model=$model;
+        $this->opis=$opis;
+        $this->url=$url;
+        $this->vlasnik=$vlasnik;
+        $this->karoserija=$karoserija;
+
+    }
+
+    function getBrend(){
+        return $this->brend;
+    }
+    function getModel(){
+        return $this->model;
+    }
+    function getOpis(){
+        return $this->opis;
+    }
+    function getUrl(){
+        return $this->url;
+    }
+    function getVlasnik(){
+        return $this->vlasnik;
+    }
+    function getKaroserija(){
+        return $this->karoserija;
+    }
+}
+
+?>
